995aeb5a0f6de4225adaa8793412c384c8d5c0c0
iwashi-yamashio <139742818+iwashi-yamashio@users.noreply.github.com>
2023-08-29 06:21:24 +0900

    Add files via upload

--- kadai_013/square.js
+++ kadai_013/square.js
@@ -1,6 +1,8 @@
-//四角形（正方形）の面積を求める
-const square = (area) => {
-  console.log(10 * 10)
+//四角形（正方形）の面積を求める（辺＊辺）
+const square = (squareSide) => {
+  console.log(squareSide * squareSide)
 }
 //関数を呼び出し、引数として面積を渡す
-square();
+square(10);
+
+
